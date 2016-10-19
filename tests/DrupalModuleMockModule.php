51a05cbabb5f8cbfca4a97c27e1b6cb6a69879dc
KeyboardCowboy <chris@lullabot.com>
2016-10-19 12:30:50 -0600

    Add comments.

--- tests/DrupalModuleMockModule.php
+++ tests/DrupalModuleMockModule.php
@@ -1,8 +1,14 @@
 <?php
+/**
+ * @file
+ * Contains \DrupalModuleMockModule.
+ */
 
 require_once __DIR__ . '/../classes/DrupalModule.php';
 
-
+/**
+ * Mock class for testing DrupalModule class.
+ */
 class DrupalModuleMockModule extends DrupalModule {
   const FIRST_VAR = 'var1';
   const SECOND_VAR = 'var2';
